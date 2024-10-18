f528e0bc057d53d089e60b75f9e250d8ed29b323
Alex Gaetano Padula <apadula@cursusdb.com>
2024-10-17 23:18:07 -0400

    - minor library header file commenting - Wal::WriteOperation to Wal::AppendOperation - Corrections on put and delete operations regarding WAL locking - Throwing TidesDBException where applicable - Public IsFlushing, IsCompacting, GetMemtable LSMT methods - On compaction, the new compacted sstables are now sorted

--- tests/lsmt_flush/lsmt_flush_test.cpp
+++ tests/lsmt_flush/lsmt_flush_test.cpp
@@ -1,6 +1,7 @@
 #include <chrono>  // Include for std::chrono::seconds
 #include <filesystem>
 #include <iostream>
+#include <set>
 #include <string>
 #include <thread>  // Include for std::this_thread::sleep_for
 #include <vector>
@@ -12,49 +13,50 @@ int main() {
     std::string directory = "./tidesdb_data";  // The directory for storing data
     std::filesystem::perms directoryPerm =
         std::filesystem::perms::owner_all | std::filesystem::perms::group_read;  // Permissions
-    int memtableFlushSize = 6 * 4;  // Flush size to trigger after 6 key-value pairs (24 bytes)
-    int compactionInterval = 22;
+    int memtableFlushSize = 6;
+    int compactionInterval = 100;
 
     try {
         // Initialize the LSMT
         auto lsmTree =
             TidesDB::LSMT::New(directory, directoryPerm, memtableFlushSize, compactionInterval);
 
-        // Insert 12 key-value pairs to trigger two flushes
-        for (int i = 0; i < 12; i++) {
-            std::vector<uint8_t> key = {
-                static_cast<uint8_t>((i >> 16) & 0xFF),  // First byte
-                static_cast<uint8_t>((i >> 8) & 0xFF),   // Second byte
-                static_cast<uint8_t>(i & 0xFF)           // Third byte
-            };
-            std::vector<uint8_t> value = {static_cast<uint8_t>(i)};
+        for (int i = 1; i <= 200; i++) {
+            std::string keyStr = std::to_string(i);
+            std::vector<uint8_t> key(keyStr.begin(), keyStr.end());
+            std::vector<uint8_t> value(keyStr.begin(), keyStr.end());
 
             lsmTree->Put(key, value);
         }
 
-        // Sleep for a while to let the background thread complete the flush
-        std::this_thread::sleep_for(std::chrono::milliseconds(755));
+        // Set of missing keys
+        std::set<std::vector<uint8_t>> missingKeys;
+        for (int i = 1; i <= 200; i++) {
+            std::string keyStr = std::to_string(i);
+            std::vector<uint8_t> key(keyStr.begin(), keyStr.end());
+            missingKeys.insert(key);
+        }
 
-        // Check for all 12 keys
-        for (int i = 0; i < 12; i++) {
-            std::vector<uint8_t> key = {
-                static_cast<uint8_t>((i >> 16) & 0xFF),  // First byte
-                static_cast<uint8_t>((i >> 8) & 0xFF),   // Second byte
-                static_cast<uint8_t>(i & 0xFF)           // Third byte
-            };
-            std::vector<uint8_t> dat = lsmTree->Get(key);
+        // Retry until all key-value pairs are found
+        while (!missingKeys.empty()) {
+            for (auto it = missingKeys.begin(); it != missingKeys.end();) {
+                std::vector<uint8_t> result = lsmTree->Get(*it);
+                if (!result.empty() && result == *it) {  // Assuming value is the same as the key
+                    it = missingKeys.erase(it);          // Remove found key from the set
+                } else {
+                    ++it;
+                }
+            }
 
-            if (dat.size() == 0) {
-                std::cerr << "Key " << i << " not found Get test failed" << std::endl;
-            } else {
-                std::cout << "Key " << i << " found Get test passed" << std::endl;
+            if (!missingKeys.empty()) {
+                std::this_thread::sleep_for(std::chrono::seconds(1));  // Wait before retrying
             }
         }
 
         lsmTree->Close();
 
         // Remove the directory
-        // std::filesystem::remove_all(directory);
+        std::filesystem::remove_all(directory);
 
         return 0;
 

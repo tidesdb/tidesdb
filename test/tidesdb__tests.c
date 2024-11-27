f840f30ad802b2755ebca30e88285d288d74c49f
Alex Gaetano Padula <me@alexpadula.com>
2024-11-26 19:49:15 -0500

    - id_gen formatting - remove _sst_extract_numeric_parts - tidesdb_cursor_init implementation

--- test/tidesdb__tests.c
+++ test/tidesdb__tests.c
@@ -874,17 +874,17 @@ void test_put_compact_get()
 
 int main(void)
 {
-    test_open_close();
-    test_create_column_family();
-    test_drop_column_family();
-    test_put();
-    test_put_get();
-    test_put_flush_get();
-    test_put_reopen_get();
-    test_put_get_delete();
-    test_txn_put_delete_get();
-    test_put_compact();
-    test_put_compact_get();
+     test_open_close();
+     test_create_column_family();
+     test_drop_column_family();
+     test_put();
+     test_put_get();
+     test_put_flush_get();
+     test_put_reopen_get();
+     test_put_get_delete();
+     test_txn_put_delete_get();
+     test_put_compact();
+     test_put_compact_get();
     /** @todo test_put_compact_get_reopen */
     /** @todo test_cursor */
     /** @todo test_concurrent_put_get */

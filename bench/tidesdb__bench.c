41d5bce5602ae66e97890026e102ff845e44b236
Alex Gaetano Padula <me@alexpadula.com>
2025-03-25 23:43:38 -0400

    tidesdb_bench remove directory after completion

--- bench/tidesdb__bench.c
+++ bench/tidesdb__bench.c
@@ -346,6 +346,8 @@ int main()
     free(key_sizes);
     free(value_sizes);
 
+    (void)_tidesdb_remove_directory("benchmark_db");
+
     printf(MAGENTA "\nCleanup completed\n" RESET);
     return 0;
 }

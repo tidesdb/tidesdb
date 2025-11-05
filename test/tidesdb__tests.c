52000f6afd9e277ce36f26a3c53dcf26065bcec5
Alex Gaetano Padula <me@alexpadula.com>
2025-11-04 20:17:09 -0500

    attempt to fix macos ci build

--- test/tidesdb__tests.c
+++ test/tidesdb__tests.c
@@ -57,7 +57,10 @@ static tidesdb_column_family_config_t get_test_cf_config(void)
         .compress_algo = COMPRESS_LZ4,
         .bloom_filter_fp_rate = 0.01,
         .enable_background_compaction = 0, /* disable for deterministic testing */
-        .use_sbha = 1                      /* enable SBHA */
+        .use_sbha = 1,                     /* enable SBHA */
+        .sync_mode = TDB_SYNC_NONE,        /* no fsync for tests */
+        .sync_interval = 0,                /* not used with SYNC_NONE */
+        .comparator_name = NULL            /* use default memcmp */
     };
     return config;
 }

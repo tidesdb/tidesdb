9d5f47e47c7281e3759661685fdea4ae58f81665
Alex Gaetano Padula <me@alexpadula.com>
2025-11-07 17:58:31 -0500

    this commit should cover absolutely all mingw warnings

--- src/compat.h
+++ src/compat.h
@@ -39,8 +39,8 @@
 #endif
 
 #if defined(__MINGW32__) || defined(__MINGW64__)
-#define TDB_SIZE_FMT     "%llu"  /* size_t promoted to unsigned long long */
-#define TDB_U64_FMT      "%I64u" /* uint64_t */
+#define TDB_SIZE_FMT     "%llu" /* size_t promoted to unsigned long long */
+#define TDB_U64_FMT      "%llu" /* uint64_t - use C99 with ANSI stdio */
 #define TDB_SIZE_CAST(x) ((unsigned long long)(x))
 #define TDB_U64_CAST(x)  ((unsigned long long)(x))
 #else
